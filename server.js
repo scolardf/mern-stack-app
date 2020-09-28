136b8c7a3eb38ef64b561be407b85a71ef479d4d
Fiach Scolard <scolardf@tcd.ie>
2020-09-28 17:32:24 +0100

    Initial Routes + Models
    Setup routes
    Set User model for db
    Set User Post request validation (no db yet)

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
 const app = express();
 
-app.get('/', (req, res) => res.send('API running...'))
+//MONGODB CONNECTION
+connectDB();
+
+//Init middleware
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API running...'));
+
+//Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
